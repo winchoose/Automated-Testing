13036c1216a63495ce04481394d1c7eb1b0987cd
winchoose <tmdxor741@naver.com>
2026-06-19 00:47:31 +0900

    init: TanStack Query 세팅 초기 세팅
    Close #15

---
+++ app/src/app/providers/query-provider.tsx
@@ -0,0 +1,15 @@
+import type { PropsWithChildren } from 'react';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { queryClient } from './query-client';
+
+export function QueryProvider({ children }: PropsWithChildren) {
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      {import.meta.env.DEV ? (
+        <ReactQueryDevtools initialIsOpen={false} />
+      ) : null}
+    </QueryClientProvider>
+  );
+}
